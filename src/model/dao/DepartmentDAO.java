aa073cbbd12730ba2ed5a05967ebdc7fe5b08224
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-05-31 17:37:58 -0300

    Implementado as classes DAO de Department e Seller

---
+++ src/model/dao/DepartmentDAO.java
@@ -0,0 +1,28 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public class DepartmentDAO {
+
+    void insert(Department obj) {
+
+    }
+
+    void update(Department obj) {
+
+    }
+
+    void deleteById(Department obj) {
+
+    }
+
+    Department findById(Integer id) {
+        return null;
+    }
+
+    List<Department> findAll() {
+        return null;
+    }
+}
---
+++ src/model/dao/SellerDAO.java
@@ -0,0 +1,30 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDAO {
+
+
+    void insert(Seller obj) {
+
+    }
+
+    void update(Seller obj) {
+
+    }
+
+    void deleteById(Seller obj) {
+
+    }
+
+    Seller findById(Integer id) {
+        return null;
+    }
+
+    List<Seller> findAll() {
+        return null;
+    }
+
+}
