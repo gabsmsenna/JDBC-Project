3390b8d7f94ac4e7564f6d51b9dfa219006bce93
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-06-05 22:46:33 -0300

    Update implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4.0000, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! new id: " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Bruce Wayne");
+        sellerDao.update(seller);
+        System.out.println("Updated! id: " + seller.getId());
     }
 }
